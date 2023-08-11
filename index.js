f76387641aa982f2038cc4dfe9e7baa2439c0153
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-08-11 17:53:58 +0200

    updated lab

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[]
+    this.length=this.items.length
 
-  add(item) {}
+  }
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>this.length-1)
+    {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+if(this.length===0){
+  throw new Error("EmptySortedList");
+}
+return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.length===0){
+  throw new Error("EmptySortedList");
+}
+return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    let sum=0
+    this.items.forEach(item=>{
+      sum+=item
+    })
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if(this.length===0){
+      throw new Error("EmptySortedList");
+    }
+    let sum= this.sum()
+    let avg=sum/this.length
+    return avg
+  }
 }
 
 module.exports = SortedList;
